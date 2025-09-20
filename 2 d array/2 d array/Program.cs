acb05fc8c5a94ad2ce686430c9aa74f5aeb706a5
Ameyingale <ameyingale15@gmail.com>
2025-09-20 12:36:38 +0530

    Add files via upload

---
+++ 2 d array/2 d array/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] a = new int[3, 2];
+        int i, j;
+
+        Console.WriteLine("enter 6 numbers");
+        for (i = 0; i < 3; i++) 
+        {
+            for (j = 0; j < 2; j++)
+            {
+                a[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        for (i = 0; i < 3; ++i)
+        {
+            for (j = 0; j < 2; ++j)
+            {
+                Console.Write(a[i, j]);
+
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
